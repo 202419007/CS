210fa1f18adb3d7801f2f7162b4efc75c05f33c5
202419007 <202419007@itc.ac.kr>
2025-05-22 18:58:54 +0900

    Create Program.cs

---
+++ week12/Program.cs
@@ -0,0 +1,23 @@
+// 전 코드랑 바뀐거 확인
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week09Homework
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormManager());
+        }
+    }
+}
