61db6b2b51e8557e2e36b765fc2b9c3c8aeada46
202419007 <202419007@itc.ac.kr>
2025-06-13 19:19:11 +0900

    Create RobotBird.cs

---
+++ ZooApp/RobotBird.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ZooApp
+{
+    class RobotBird : Animal, IRobot
+    {
+        public RobotBird
+           (string name, COLOR color)
+           : base(name, color)
+        {
+
+        }
+
+        int _batteryLevel;
+        public int BatteryLevel
+        {
+            get { return _batteryLevel; }
+            set {
+                if (value > 1000) {
+                    _batteryLevel = 1000;
+                } else {
+                    _batteryLevel = value;
+                }
+            }
+        }
+
+        public void Charge()
+        {
+            BatteryLevel = 1000;
+        }
+
+        public override string ToString()
+        {
+            return $"ROBOTBIRD:{Name}";
+        }
+
+        public string Fly(int count)
+        {
+            string retValue = "";
+            for (int i = 0; i < count; i++) {
+                retValue += "푸드득~";
+            }
+            return retValue;
+        }
+
+        protected override bool AddLevel(int level)
+        {
+            if (_level + level <= 50) {
+                _level += level;
+                return true;
+            } else {
+                _level = 100;
+                return false;
+            }
+        }
+    }
+}
